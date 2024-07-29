c18c7c343bef0159eee232c1c47ebd6af3b1ab91
eunhye han <hellou8363@gmail.com>
2024-07-30 08:47:20 +0900

    Fix: 오타 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
 import starFill from '@/assets/star-fill.svg';
-import halfStart from '@/assets/half-star.svg';
+import halfStar from '@/assets/half-star.svg';
 import thumbsUpFill from '@/assets/thumbs-up-fill.svg';
 import thumbsUpBlank from '@/assets/thumbs-up-blank.svg';
 import thumbsDownFill from '@/assets/thumbs-down-fill.svg';
 import thumbsDownBlank from '@/assets/thumbs-down-blank.svg';
 
-export { starFill, halfStart, thumbsUpFill, thumbsUpBlank, thumbsDownFill, thumbsDownBlank };
+export { starFill, halfStar, thumbsUpFill, thumbsUpBlank, thumbsDownFill, thumbsDownBlank };
