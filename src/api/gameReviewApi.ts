a370768278c50fc1b32983ebf56dc5bc2cd08b79
eunhye han <hellou8363@gmail.com>
2024-07-30 10:11:01 +0900

    Fix: 오타 수정

--- src/api/gameReviewApi.ts
+++ src/api/gameReviewApi.ts
@@ -1,25 +1,25 @@
-import { fromourGamerClient } from './FourGamerClient';
+import { fourGamerClient } from './FourGamerClient';
 
 export const getGameReviewList = async (page: number, size: number) => {
-  const response = await fromourGamerClient.get(`/api/v1/game-reviews?page=${page}&size=${size}`);
+  const response = await fourGamerClient.get(`/api/v1/game-reviews?page=${page}&size=${size}`);
 
   return response.data;
 };
 
 export const getGameReview = async (gameReviewId: string = '') => {
-  const response = await fromourGamerClient.get(`/api/v1/game-reviews/${gameReviewId}`);
+  const response = await fourGamerClient.get(`/api/v1/game-reviews/${gameReviewId}`);
 
   return response.data;
 };
 
 export const updateGameReview = async (gameReviewId: string) => {
-  const response = await fromourGamerClient.put(`/api/v1/game-reviews/${gameReviewId}`);
+  const response = await fourGamerClient.put(`/api/v1/game-reviews/${gameReviewId}`);
 
   return response.data;
 };
 
 export const deleteGameReview = async (gameReviewId: string) => {
-  const response = await fromourGamerClient.delete(`/api/v1/game-reviews/${gameReviewId}`);
+  const response = await fourGamerClient.delete(`/api/v1/game-reviews/${gameReviewId}`);
 
   return response.data;
 };
