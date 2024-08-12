d0967160687f1059219e6ea8ba11dfe1099431b5
eunhye han <hellou8363@gmail.com>
2024-08-12 09:16:03 +0900

    Fix: 브라우저를 닫을 때 서버에 연결 종료를 알리는 코드 추가

--- src/components/layout/WebsocketConnection.tsx
+++ src/components/layout/WebsocketConnection.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef } from 'react';
 import { getMemberInfo } from '../../api/member';
-import { Client } from '@stomp/stompjs';
+import { Client, StompSubscription } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 const WebsocketConnection = () => {
   const clientRef = useRef<Client>();
-
   const tokenRef = useRef(localStorage.getItem('accessToken') || '');
+  const subscriptionRef = useRef<StompSubscription>();
+  const subjectId = useRef<string>();
 
   const webSocketConnection = (userId: string) => {
     const stompClient = new Client({
@@ -14,14 +15,17 @@ const WebsocketConnection = () => {
       connectHeaders: {
         token: tokenRef.current,
       },
-      // debug: (msg) => console.log(msg),
+      debug: (msg) => console.log(msg),
       reconnectDelay: 500000,
       onConnect: () => {
         console.log('Connected');
 
-        stompClient.subscribe(`/sub/notification/${userId}`, (notification) => {
-          console.log('notification: ', notification.body);
-        });
+        subscriptionRef.current = stompClient.subscribe(
+          `/sub/notification/${userId}`,
+          (notification) => {
+            console.log('notification: ', notification.body);
+          }
+        );
       },
     });
 
@@ -33,12 +37,36 @@ const WebsocketConnection = () => {
   const initialize = async () => {
     const userInfo = await getMemberInfo(tokenRef.current);
     webSocketConnection(userInfo.id);
+    subjectId.current = userInfo.id;
+  };
+
+  // 웹소켓 연결 끊기 & 채팅창 닫기
+  const webSocketDisConnection = () => {
+    clientRef.current?.publish({
+      destination: '/pub/notification',
+      body: JSON.stringify({
+        type: 'EXIT',
+        subjectId: subjectId.current,
+        targetId: subjectId.current,
+        message: `${subjectId}님이 로그아웃 또는 브라우저를 종료했습니다.`,
+        createdAt: new Date(),
+      }),
+    });
+
+      subscriptionRef.current?.unsubscribe();
+      clientRef.current?.deactivate();
   };
 
   useEffect(() => {
     if (tokenRef.current) {
       initialize();
     }
+
+    window.addEventListener('beforeunload', webSocketDisConnection);
+
+    return () => {
+      window.removeEventListener('beforeunload', webSocketDisConnection);
+    };
   }, [tokenRef]);
 
   return <></>;
