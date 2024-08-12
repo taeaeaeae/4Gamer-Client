236a0983f580cd97f72fbb9d1958eb1540a71f0e
강백호 <166815465+KangBaekho10@users.noreply.github.com>
2024-08-12 20:49:18 +0900

    Fix: 쓸모없는 import문 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,7 +14,6 @@ import ChannelModify from "./pages/channels/ModifyChannel"
 import ChannelAdminPage from "./pages/channels/ChannelAdminPage"
 import BoardCreate from "./pages/channels/CreateBoard"
 import { GoogleLogin } from './pages/GoogleLogin.page';
-import UserProfile from './components/UserProfile';
 
 const router = createBrowserRouter([
   {
