61ba8c47e822b48d4717db4f1ee4cc8b541ab5d5
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-08-19 19:43:45 +0900

    Update MemberContainer.tsx

--- src/components/MemberContainer.tsx
+++ src/components/MemberContainer.tsx
@@ -175,7 +175,7 @@ export function MemberContainer() {
             <Text><strong>닉네임 : </strong> {userData.name}</Text>
             <br />
             <div style={{ display: 'flex', gap: '1rem', marginTop: '1rem' }}>
-              <Button onClick={userData.name.includes('google') ? googleCheck : () => setModalOpen(prev => ({ ...prev, nickname: true }))} color="blue" style={{ flex: 1 }}>
+              <Button onClick={!(userData.name.includes('google')) ? () => setModalOpen(prev => ({ ...prev, nickname: true }))} color="blue" style={{ flex: 1 } :  googleCheck }>
                 닉네임 변경
               </Button>
               <Button onClick={userData.name.includes('google') ? googleCheck : () => setModalOpen(prev => ({ ...prev, password: true }))} color="blue" style={{ flex: 1 }}>
